6b10b81355afbaac6ac3f55f4b0e36a5b03b4312
Smit Patel <smit@cityflo.com>
2019-12-04 00:25:14 +0530

    Improved with mapping

--- day-03-crossed-wires/src/main.rs
+++ day-03-crossed-wires/src/main.rs
@@ -24,47 +24,52 @@ fn main() {
         .map(|line| line.split(",").collect())
         .collect();
 
-    let path_1 = get_path(&ans[0]);
-    let path_2 = get_path(&ans[1]);
+    let points_1 = get_path(&ans[0]);
+    let points_2 = get_path(&ans[1]);
 
-    let line_1 = get_coords(&path_1);
-    let line_2 = get_coords(&path_2);
+    let path_1 = get_coords(&points_1);
+    let path_2 = get_coords(&points_2);
 
-    let common: HashSet<_> = line_1.intersection(&line_2).collect();
+    let common: HashSet<_> = path_1.intersection(&path_2).collect();
 
     let ans_1 = common.iter().map(|(x, y)| x.abs() + y.abs()).min();
 
     println!("Part 1: Ans -> {:?}", ans_1);
 
-    let dist_1 = dist_map(&path_1);
-    let dist_2 = dist_map(&path_2);
-
-    let ans_2 = common.iter().map(|k| dist_1.get(&k).unwrap() + dist_2.get(&k).unwrap()).min();
+    let ans_2 = common
+        .iter()
+        .map(|k| points_1.get(&k).unwrap() + points_2.get(&k).unwrap())
+        .min();
 
     println!("Part 2: Ans -> {:?}", ans_2);
 }
 
-fn get_coords(path: &Vec<(i32, i32)>) -> HashSet<(i32, i32)> {
-    path.iter().cloned().collect()
+fn get_coords(path: &HashMap<(i32, i32), usize>) -> HashSet<&(i32, i32)> {
+    path.keys().collect()
 }
 
-fn get_path(path: &Vec<&str>) -> Vec<(i32, i32)> {
+fn get_path(path: &Vec<&str>) -> HashMap<(i32, i32), usize> {
     let mut x = 0;
     let mut y = 0;
+    let mut length = 0;
 
-    let mut path_list = Vec::new();
+    let mut path_map = HashMap::new();
 
     for i in path {
         let (s_x, s_y, dist) = get_dir(i);
         for _ in 0..dist {
             x += s_x;
             y += s_y;
+            length += 1;
+            let k = (x, y);
 
-            path_list.push((x, y));
+            if !path_map.contains_key(&k) {
+                path_map.insert((x, y), length);
+            }
         }
     }
 
-    path_list
+    path_map
 }
 
 fn get_dir(dir: &str) -> (i32, i32, i32) {
@@ -79,18 +84,6 @@ fn get_dir(dir: &str) -> (i32, i32, i32) {
     }
 
     (step_x, step_y, dir[1..].parse().unwrap())
-}
-
-fn dist_map(path: &Vec<(i32, i32)>) -> HashMap<(i32, i32), usize> {
-    let mut dist = HashMap::new();
-
-    for (ind, &x) in path.iter().enumerate() {
-        if !dist.contains_key(&x) {
-            dist.insert(x, ind + 1);
-        }
-    }
-
-    dist
 }
 
 impl Config {
